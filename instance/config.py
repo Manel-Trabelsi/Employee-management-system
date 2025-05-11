667b7cdc0d9fb01d529a3a1eb8e1aadad8d5ac8c
maamoun <mribahmaamoun@gmail.com>
2025-05-11 20:42:25 +0100

    fix auth

--- instance/config.py
+++ instance/config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql://squaduser:squadpass@db:3306/squadmaster'
+SQLALCHEMY_DATABASE_URI = 'mysql://squaduser:squadpass@db.app.svc.cluster.local:3306/squadmaster'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'your-secret-key'
